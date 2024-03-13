e17ac121ef5afcc696831dd84a3d57c84580563a
Coding Carson <coding@carson.rip>
2024-03-13 00:47:13 -0700

    •New form "Highlight_Overlay.vb" used by new module "MonitorManagement_Module.vb"

---
+++ Highlight_Overlay.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Highlight_Overlay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As ComponentResourceManager = New ComponentResourceManager(GetType(Highlight_Overlay))
+        Panel1 = New Windows.Forms.Panel()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Black
+        Panel1.Location = New Point(9, 9)
+        Panel1.Margin = New System.Windows.Forms.Padding(0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(332, 182)
+        Panel1.TabIndex = 0
+        ' 
+        ' Highlight_Overlay
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = Windows.Forms.AutoScaleMode.Font
+        AutoValidate = Windows.Forms.AutoValidate.Disable
+        BackColor = Color.Cyan
+        ClientSize = New Size(350, 200)
+        ControlBox = False
+        Controls.Add(Panel1)
+        Enabled = False
+        FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Highlight_Overlay"
+        ShowInTaskbar = False
+        SizeGripStyle = Windows.Forms.SizeGripStyle.Hide
+        StartPosition = Windows.Forms.FormStartPosition.Manual
+        Text = "True Stretched - Monitor Highlight"
+        TransparencyKey = Color.Black
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Windows.Forms.Panel
+End Class
---
+++ Highlight_Overlay.vb
@@ -0,0 +1,28 @@
+﻿Public Class Highlight_Overlay
+
+    ' Ensure Overlay Panel doesn't set to active when it's shown
+    Protected Overrides ReadOnly Property ShowWithoutActivation As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Private Sub Highlight_Overlay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Initial adjustments of Overlay Panel
+        AdjustPanelSizeAndLocation()
+
+    End Sub
+
+    Private Sub Highlight_Overlay_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        AdjustPanelSizeAndLocation()
+    End Sub
+
+    Private Sub AdjustPanelSizeAndLocation()
+        ' Ensuring Panel1 is always 5px smaller around the border compared to OverlayForm
+        With Panel1
+            .Location = New Point(5, 5) ' 5px offset from the top-left corner
+            .Size = New Size(Me.ClientSize.Width - 10, Me.ClientSize.Height - 10) ' 5px offset from each side
+        End With
+    End Sub
+End Class
