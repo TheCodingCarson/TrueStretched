392b712a28abb67f4c57ce8a8791ef22cbcf346a
Coding Carson <coding@carson.rip>
2024-03-14 14:19:32 -0700

    •New Module "GlobalVariables.vb" for all the global variables
    •New Module "GlobalVariables.vb" for all the global variables

---
+++ Modules/GlobalVariables.vb
@@ -0,0 +1,8 @@
+﻿Module GlobalVariables
+
+    ' Module Used to Declare Global Veriables
+
+    Public DevBuild As Boolean = False ' Used For Enabling DevMode
+    Public AutoDisable As Boolean = False ' Used To Automatically Disable True Stretched On Launch
+
+End Module
