8bbdad2412e3cdafaf7ec51ed01edb71f836fb0e
Coding Carson <coding@carson.rip>
2024-08-06 09:22:10 -0700

    •New Settings for Custom Delayed Stretching Time
    New Settings for Valorant's Delayed Stretching so users can pick preferred await time for their system.

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -279,6 +279,18 @@ Namespace My
             End Get
             Set
                 Me("ValorantDelayStretchFix") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("45")>  _
+        Public Property ValorantDelayStretchedTime() As Integer
+            Get
+                Return CType(Me("ValorantDelayStretchedTime"),Integer)
+            End Get
+            Set
+                Me("ValorantDelayStretchedTime") = value
             End Set
         End Property
     End Class
