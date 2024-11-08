1b394a63235e44b75392e33daf74a999669f9b13
Coding Carson <coding@carson.rip>
2024-11-08 03:50:13 -0800

    Removed Enabling/Disabling Delaying Stretch for Valorant

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -267,18 +267,6 @@ Namespace My
             End Get
             Set
                 Me("ValorantFirstTime") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
-        Public Property ValorantDelayStretchFix() As Boolean
-            Get
-                Return CType(Me("ValorantDelayStretchFix"),Boolean)
-            End Get
-            Set
-                Me("ValorantDelayStretchFix") = value
             End Set
         End Property
         
