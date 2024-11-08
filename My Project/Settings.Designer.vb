a108a50fc9c0df139fbdeed08e8df55f437a3652
Coding Carson <coding@carson.rip>
2024-11-08 04:07:07 -0800

    •Changed Version Number •Changed to Beta Build

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -200,7 +200,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property BetaBuild() As Boolean
             Get
                 Return CType(Me("BetaBuild"),Boolean)
@@ -212,7 +212,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("a")>  _
         Public Property BetaLetter() As String
             Get
                 Return CType(Me("BetaLetter"),String)
