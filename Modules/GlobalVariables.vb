e1280ac7aae749fbfc25262173e3699460f42b92
Coding Carson <coding@carson.rip>
2024-11-09 01:41:54 -0800

    Added Global Variable "StretchedEnabled"

--- Modules/GlobalVariables.vb
+++ Modules/GlobalVariables.vb
@@ -2,9 +2,10 @@
 
     ' Module Used to Declare Global Veriables
 
-    Public DevBuild As Boolean = False ' Used For Enabling DevMode
-    Public AutoDisable As Boolean = False ' Used To Automatically Disable True Stretched On Launch
-    Public OverrideNative As Boolean = False ' Used To Override Native Display Resolution
-    Public OverrideNativeRes As String = "" ' ^ Resolution used by Override
+    Public DevBuild As Boolean = False         ' Used For Enabling DevMode
+    Public StretchedEnabled As Boolean = False ' Used for checking if Stretched is currently enabled
+    Public AutoDisable As Boolean = False      ' Used To Automatically Disable True Stretched On Launch
+    Public OverrideNative As Boolean = False   ' Used To Override Native Display Resolution
+    Public OverrideNativeRes As String = ""    ' ^ Resolution used by Override
 
 End Module
