a2eefe36a1e29281164cf763950b8edfacc579c8
Coding Carson <coding@carson.rip>
2024-11-09 01:44:18 -0800

    New Helper "CheckForWindow" Added
    Moved function from Form1 for checking for windows by name to it's own helper

---
+++ Helpers/CheckForWindow_Helper.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.InteropServices
+
+Module CheckForWindow_Helper
+    ' {-Summary-}
+    '
+    ' Used to check if the program is running with administrator privleages
+    '
+    ' Usage:
+    '        • CheckForWindow("WindowNameAsString") -- Checks to see if a window is currently running with the same name
+    '        • FindWindow(Nothing, "WindowNameAsString") -- Checks for processid of window running with the same name
+    '
+    ' Return:
+    '         • CheckForWindow() -- True/False if Window was found with name
+    '         • FindWindow()     -- Returns Window Handle as IntPtr
+    '
+    ' {-Summary-}
+
+    Public Function CheckForWindow(windowName As String)
+        Dim hwnd As IntPtr = FindWindow(Nothing, windowName)
+        If hwnd = IntPtr.Zero Then
+            Return False 'Window Not Found
+        Else
+            Return True ' Window was Found
+        End If
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Unicode)>
+    Public Function FindWindow(lpClassName As String, lpWindowName As String) As IntPtr
+    End Function
+End Module
